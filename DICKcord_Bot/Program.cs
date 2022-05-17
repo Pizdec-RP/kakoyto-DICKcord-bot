8cc220fd3a6592d377a09fa859da5b30a72edcb4
Pizdec-RP <qm0801u777@gmail.com>
2022-05-17 12:45:45 +0300

    asdasdasd

--- DICKcord_Bot/Program.cs
+++ DICKcord_Bot/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord.WebSocket;
+using Discord;
+class Zalypa
+{
+    public static DiscordSocketClient? client;
+    static void Main(String[] args)
+    {
+        runBot("you token here").GetAwaiter().GetResult();
+    }
+
+    public static async Task runBot(String token)
+    {
+        client = new DiscordSocketClient();
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+        await client.SetGameAsync("ахуеть это говно запустилось на C#");
+        client.MessageReceived += HMessage;
+        await Task.Delay(Timeout.Infinite);
+    }
+
+    private static Task HMessage(SocketMessage ctx)
+    {
+        if (ctx.Author.IsBot) return Task.CompletedTask;
+        if (ctx.Content.Equals("huy"))
+        {
+            ctx.Channel.SendMessageAsync("pizda");
+        }
+        return Task.CompletedTask;
+    }
+}
